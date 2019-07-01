5086958e34d91d11b4e2ef695110c00248c6cbb8
TheHellBox <thehellbox11@gmail.com>
2019-07-01 18:44:53 +0500

    Camera controller

---
+++ src/game/camera_controller.rs
@@ -0,0 +1,55 @@
+use crate::components::{Camera, Transform};
+use crate::game::PlayerController;
+use nalgebra::center;
+use nalgebra::distance;
+
+pub struct CameraController;
+
+impl<'a> specs::System<'a> for CameraController {
+    type SystemData = (
+        specs::WriteStorage<'a, Camera>,
+        specs::ReadStorage<'a, PlayerController>,
+        specs::WriteStorage<'a, Transform>,
+    );
+    fn run(&mut self, (mut cameras, controllers, mut transforms): Self::SystemData) {
+        use specs::Join;
+
+        let mut player_transforms = vec![];
+
+        for (controller, transform) in (&controllers, &transforms).join() {
+            player_transforms.push(transform.clone());
+        }
+
+        for (camera, camera_transform) in (&mut cameras, &mut transforms).join() {
+            let mut camera_min_vec = na::Point2::new(999.0, 999.0);
+            let mut camera_max_vec = na::Point2::new(0.0, 0.0);
+
+            for transform in &player_transforms {
+                if transform.position.x > camera_max_vec.x {
+                    camera_max_vec.x = transform.position.x;
+                }
+                if transform.position.y > camera_max_vec.y {
+                    camera_max_vec.y = transform.position.y;
+                }
+                if transform.position.x < camera_min_vec.x {
+                    camera_min_vec.x = transform.position.x;
+                }
+                if transform.position.y < camera_min_vec.y {
+                    camera_min_vec.y = transform.position.y;
+                }
+            }
+
+            let camera_middle_point = center(&camera_max_vec, &camera_min_vec);
+            let distance = distance(&camera_max_vec, &camera_min_vec);
+            let ratio = 1024.0 / 768.0;
+            let fov = std::f32::consts::PI / 2.0;
+            let far = (-distance * 0.7 / ratio) / (fov / 2.0).tan() - 3.0;
+            println!("{}", far);
+            camera_transform.position = na::Point3::new(
+                -camera_middle_point.x,
+                camera_middle_point.y,
+                far,
+            );
+        }
+    }
+}
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,3 +1,4 @@
+pub mod camera_controller;
 pub mod map_loader;
 pub mod player;
 
@@ -9,11 +10,17 @@ pub type PlayerController = player::PlayerController;
 pub fn register_systems<'a>(
     builder: specs::DispatcherBuilder<'a, 'a>,
 ) -> specs::DispatcherBuilder<'a, 'a> {
-    builder.with(
-        player::PlayerControllerSystem,
-        "Player Controller System",
-        &[],
-    )
+    builder
+        .with(
+            player::PlayerControllerSystem,
+            "Player Controller System",
+            &[],
+        )
+        .with(
+            camera_controller::CameraController,
+            "Camera Controller",
+            &[],
+        )
 }
 
 pub fn register_default(world: &mut specs::World) {
@@ -25,7 +32,7 @@ pub fn setup_scene(world: &mut specs::World) {
 
     let _camera1 = world
         .create_entity()
-        .with(Camera::new((1024.0, 768.0), 3.0))
+        .with(Camera::new((1024.0, 768.0), 10.0))
         .with(Transform {
             position: na::Point3::new(0.0, 0.0, -2.0),
             rotation: na::UnitQuaternion::from_euler_angles(std::f32::consts::PI, 0.0, 0.0),
